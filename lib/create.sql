0f53a6c98e055ed12302904590b938ace4775e9b
mkenane <morkenan@hotmail.com>
2018-03-21 09:54:19 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER
+);
+
+-- check(gender = "F" or gender = "M")
+-- create table MyTable
+-- (
+--     name string check(name = "car" or name = "bike" or name = "van")
+-- );
+--
+--
+-- column	type
+-- id	integer
+-- name	text
+-- age	integer
+-- gender	char(1)(The choices would be "M" or "F")
+-- color	text
+-- temperament	text
+-- alive	boolean
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'friendly', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'black', 'slow', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 18, 'F', 'white', 'passive', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'F', 'brown', 'Aggressive', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'M', 'white', 'playful', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 13, 'M', 'brown', 'grumpy', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'black', 'chill', 1);
+INSERT INTO Bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, 'M', 'white', 'asshole', 1);
